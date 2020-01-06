486a5d81f52f0626460bda14b1d18d12c4e3043a
1434412784@qq.com <GitHubXudacheng8->
2020-01-06 22:41:47 +0800

    参考视频完成一个自定义的文件过滤器

--- src/test/java/com/github/hcsp/polymorphism/FileFilterTest.java
+++ src/test/java/com/github/hcsp/polymorphism/FileFilterTest.java
@@ -1,11 +1,13 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
