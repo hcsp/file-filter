5bf61d36d0aa1e748302b2dd55e54d9ead791bb9
zj <woaiwrf@qq.com>
2022-06-23 12:48:18 +0800

    filter

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,11 +1,11 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
-public class FileFilter {
+public class FileFilter extends SimpleFileVisitor {
     public static void main(String[] args) throws IOException {
         Path projectDir = Paths.get(System.getProperty("user.dir"));
         Path testRootDir = projectDir.resolve("test-root");
@@ -17,12 +17,22 @@ public class FileFilter {
         System.out.println(filteredFileNames);
     }
 
+    @Override
+    public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
+
+        return FileVisitResult.CONTINUE;
+    }
+
     /**
      * 实现一个按照扩展名过滤文件的功能
      *
      * @param rootDirectory 要过滤的文件夹
-     * @param extension 要过滤的文件扩展名，例如 .txt
+     * @param extension     要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        FileFilterVisitor visitor = new FileFilterVisitor(extension);
+        Files.walkFileTree(rootDirectory, visitor);
+        return visitor.getResults();
+    }
 }
